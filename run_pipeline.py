0f9a738d8e4652cbd12402a08b52fcc54969178a
Karthikeya <karthikeya16082@gmail.com>
2024-10-31 15:28:44 +0530

    added mlflow

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,6 +1,7 @@
 from zenml import pipeline, step
 from pipelines.main_pipeline import pipeline_dev
-
+from zenml.client import Client
 if __name__ == "__main__":
+    print(Client().active_stack.experiment_tracker.get_tracking_uri())
     pipeline_dev(data_path="Housing.csv")
-    
+ # mlflow ui --backend-store-uri "file:C:\Users\karth\AppData\Local\Packages\PythonSoftwareFoundation.Python.3.9_qbz5n2kfra8p0\LocalCache\Roaming\zenml\mlruns"  
--- steps/model_training.py
+++ steps/model_training.py
@@ -3,11 +3,17 @@ from models.model_train import training_model
 import pandas as pd
 from typing import Tuple
 from sklearn.tree import DecisionTreeRegressor
+from zenml.client import Client
+import mlflow.sklearn
+from mlflow.models.signature import infer_signature
 
-@step
+experiment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=experiment_tracker.name)
 def training(x: pd.DataFrame, y: pd.DataFrame) -> DecisionTreeRegressor:
     dt = training_model()
     dtree = dt.model_training(x, y)
+    mlflow.sklearn.log_model(artifact_path="model", sk_model=dtree, signature=infer_signature(x), registered_model_name="DecisionTreeRegressor")
     return dtree
 
 def model_create(dr: DecisionTreeRegressor) -> DecisionTreeRegressor:
